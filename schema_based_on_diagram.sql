88b56cc82d839ad0fe585b8567acaf01614f355d
zeff96 <zeffo1975@gmail.com>
2023-05-09 12:13:36 +0300

    Add medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,11 @@ CREATE TABLE patients(
   date_of_birth DATE,
   PRIMARY KEY(id)
 );
+
+CREATE TABLE medical_histories(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP,
+  patient_id INT,
+  status VARCHAR,
+  PRIMARY KEY(id)
+);
